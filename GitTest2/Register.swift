a8390b33203650510ce2f076c678470b055b37b5
yarob <yarob.hm@gmail.com>
2017-10-31 20:28:08 +0300

    adding the register class

---
+++ GitTest2/Register.swift
@@ -0,0 +1,13 @@
+//
+//  Register.swift
+//  GitTest2
+//
+//  Created by يعرب المصطفى on 10/31/17.
+//  Copyright © 2017 yarob. All rights reserved.
+//
+
+import Foundation
+class Register
+{
+    
+}
