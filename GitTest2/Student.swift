0fcf9bd91c2937b9bb0351c09065d79f56a94396
yarob <yarob.hm@gmail.com>
2017-10-31 20:50:12 +0300

    adding the student class

---
+++ GitTest2/Student.swift
@@ -0,0 +1,13 @@
+//
+//  Student.swift
+//  GitTest2
+//
+//  Created by يعرب المصطفى on 10/31/17.
+//  Copyright © 2017 yarob. All rights reserved.
+//
+
+import Foundation
+class Student
+{
+    
+}
